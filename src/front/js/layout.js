c371639fd993b3812fd077041a901b5b68abddca
Jhonny Mendoza <jhomen11@gmail.com>
2021-12-16 01:39:07 +0000

    Creación de Formulario de registro y route front

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import {Registro} from "./pages/registro";
 
 //create your first component
 const Layout = () => {
@@ -25,8 +26,8 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/registro">
+							<Registro />
 						</Route>
 						<Route exact path="/single/:theid">
 							<Single />
--- src/front/js/pages/registro.js
+++ src/front/js/pages/registro.js
@@ -1,16 +1,18 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext"
-import "../../styles/home.css";
+import React from "react";
 
-export const Home = () => {
-  const { store, actions } = useContext(Context);
+export const Registro = () => {
+  
 
   return (
     <div className=" container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6 col-lg-4">
-        <h2 className="text-center display-3">Login</h2>
+            <h2 className="text-center display-3">Sign Up</h2>
           <form className="mt-4">
+          <div className="mb-2">
+				<label className="form-label">Name</label>
+				<input type="text" className="form-control"/>
+			</div>
             <div className="mb-2">
 				<label className="form-label">Email</label>
 				<input type="email" className="form-control"/>
@@ -21,7 +23,7 @@ export const Home = () => {
 			</div>
 			<div className="d-grid mt-4">
 				<button className="btn btn-success">
-					Enviar
+					Register
 				</button>
 			</div>
           </form>
